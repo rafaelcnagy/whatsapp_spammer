b41e966c123170bb25a9ebe630c0a7e7c986867d
rafaelcnagy <rafaelcnagy@hotmail.com>
2019-05-07 21:48:44 -0300

    Contador de mensagens adicionado

--- crawler.py
+++ crawler.py
@@ -5,7 +5,7 @@ import time
 contact_name = 'Pedro pontes'
 message = '''TRAGA MINHA BOLSINHA PORRA!!!!
                 se bloquear é puta'''
-delay = 60 #in seconds
+delay = 300 #in seconds
 
 browser = webdriver.Firefox()
 browser.get('https://web.whatsapp.com/')
@@ -26,12 +26,15 @@ except NoSuchElementException:
 else:
     elem.send_keys(contact_name + Keys.RETURN)
 
-print(contact_name + 'selecionado')
+print(contact_name + ' selecionado')
 
+count = 0
 try:
     while True:
         elem = browser.find_element_by_class_name('_2S1VP')
         elem.send_keys(message + Keys.RETURN)
+        count += 1
+        print(' --- Voce enviou a {}^a mensagem'.format(count))
         time.sleep(delay)
 except KeyboardInterrupt:
     print('Interrompido!')
