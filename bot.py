6fa5cf63d8d596ace58e7cfd2906e39151e271b2
rafaelcnagy <rafaelcnagy@hotmail.com>
2019-05-08 11:39:09 -0300

    erro corrigido

--- bot.py
+++ bot.py
@@ -5,13 +5,13 @@ import random
 import string
 
 contact_name = 'Pedro pontes'
-message = ['TRAGA MINHA BOLSINHA PORRA!!!!', 'se bloquear é puta', 'BORA MERMAO, DEVOLVE ESSA MERDA', 'se nao devolver amanha, seu pau vai cair', 'block = 50 conto!', 'while (pedro == gado)<br>   bot_active = true;', 'só traz essa porra, tamo junto man']
-min_delay = 10 #in seconds
-max_delay = 20 #in seconds
+message = ['TRAGA MINHA BOLSINHA PORRA!!!!', 'se bloquear é puta', 'BORA MERMAO, DEVOLVE ESSA MERDA', 'se nao devolver amanha, seu pau vai cair', 'block = 50 conto!', 'while (pedro == gado)\r   bot_active = true;']
+min_delay = 100 #in seconds
+max_delay = 200 #in seconds
 min_interval = 480 #in seconds
 max_interval = 600 #in seconds
-min_count2interval = 100
-max_count2interval = 150
+min_count2interval = 20
+max_count2interval = 26
 
 browser = webdriver.Firefox()
 browser.get('https://web.whatsapp.com/')
@@ -62,7 +62,7 @@ try:
             count = 0
             print(' intervalo de {} segundos'.format(count2interval))
             count2interval = random.randint(min_count2interval, max_count2interval)
-            time.sleep(min_interval, max_interval)
+            time.sleep(randon.randint(min_interval, max_interval))
         else:
             time.sleep(random.randint(min_delay, max_delay))
         count += 1
